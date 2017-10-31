c2fc7c9171a7826c19d25cfc4695db0280ec0c5d
Axel <latiosaxe@gmail.com>
2017-10-30 22:42:06 -0600

    Details

--- src/components/elements/Contact.vue
+++ src/components/elements/Contact.vue
@@ -4,6 +4,8 @@
             <p>If you want to know more about me or just talk, send me a message at <a href="mailto:latiosaxe@gmail.com">latiosaxe@gmail.com</a></p>
             <p class="twitter">Or send me a Tweet: <a href="https://twitter.com/latiosaxe" target="_blank">@latiosaxe</a></p>
             <p>Thank you for your time :)</p>
+
+            <p class="small"><a href="https://github.com/latiosaxe/portfolio-2017" target="_blank">Check this website on Github</a></p>
         </div>
         <div class="contact__bottom">
             <p>
@@ -74,6 +76,15 @@
                 color: #487ac6;
                 a{
                     color: #487ac6;
+                }
+            }
+            p.small{
+                font-size: 1rem;
+                a{
+                    font-family: $font-1;
+                    &:hover{
+                         text-decoration: underline;
+                     }
                 }
             }
        }
